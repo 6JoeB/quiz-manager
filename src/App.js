06825c7cd1d911ed2d475136babdb9fc3e89cc6f
Joe Ball <joe.ball@bglgroup.co.uk>
2020-10-16 16:11:36 +0100

    added bootstrap and created nav bar

--- src/App.js
+++ src/App.js
@@ -5,7 +5,11 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        
+        <nav class="nav">
+          <a class="nav-link" href="#">Home</a>
+          <a class="nav-link" href="#">logout</a>
+        </nav>
+        <button type="button" class="btn btn-primary">Primary</button>
       </header>
     </div>
   );
