671a225f29406ee304761a3ad76d1b1cacb5e280
Joe Ball <joe.ball@bglgroup.co.uk>
2020-10-19 15:54:22 +0100

    added login and logout functionality using Auth0

--- src/App.js
+++ src/App.js
@@ -1,18 +1,33 @@
 import React from 'react';
 import './App.css';
+import LoginButton from './components/LoginButton';
+import LogoutButton from './components/LogoutButton';
+import Profile from './components/Profile';
+import { useAuth0 } from '@auth0/auth0-react';
 
 function App() {
+  const { isLoading } = useAuth0();
+
+  if (isLoading) return <div>Loading... </div>
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <nav class="nav">
-          <a class="nav-link" href="#">Home</a>
-          <a class="nav-link" href="#">logout</a>
-        </nav>
-        <button type="button" class="btn btn-primary">Primary</button>
-      </header>
-    </div>
+    <>
+      <LoginButton/>
+      <LogoutButton/>
+      <Profile/>
+    </>
   );
 }
 
 export default App;
+
+
+
+/* <div>
+<header className="App-header">
+  <nav class="nav">
+    <a class="nav-link" href="#">Home</a>
+    <a class="nav-link" href="#">logout</a>
+  </nav>
+</header>
+</div> */
