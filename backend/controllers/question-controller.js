eea77dce5161e873f9919e117811d9bfe9cb91e8
Joe Ball <joe.ball@bglgroup.co.uk>
2020-10-28 16:17:40 +0000

    added router linking question controller methods

---
+++ backend/controllers/question-controller.js
@@ -0,0 +1,160 @@
+import Question, { findOne, findMany, findOneAndDelete, find } from '../models/question-model';
+
+createQuestion = (req, res) => {
+    const body = req.body;
+
+    if (!body) {
+        return res.status(400).json({
+            succes: false,
+            error: 'You must provide a body',
+        })
+    }
+
+    const question = new Question(body);
+
+    if (!question) {
+        return res.status(400).json({ success: false, error: err});
+    }
+
+    question.save().then(() => {
+        return res.status(201).json({
+            success: true,
+            id: question._id,
+            message: 'Question created',
+        })
+    }).catch(error => {
+        return res.status(400).json({
+            error,
+            message: 'Question not created',
+        })
+    })
+}
+
+updateQuestion = async (req, res) => {
+    const body = req.body;
+
+    if (!body) {
+        return res.status(400).json({
+            succes: false,
+            error: 'You must provide a body',
+        })
+    }
+
+    findOne({ _id: req.params.id }, (err, question) => {
+        if (err) {
+            return res.status(404).json({
+                err,
+                message: 'Question not found',
+            })
+        }
+        question.question = body.question;
+        question.answer = body.answer;
+        question.save().then(() => {
+            return res.status(200).json({
+                success: true,
+                id: question._id,
+                message: 'Question updated',
+            })
+        }).catch(error => {
+            return res.status(404).json({
+                error,
+                message: 'Question not updated'
+            });
+        });
+    });
+}
+
+updateQuizName = async (req, res) => {
+    const body = req.body;
+
+    if (!body) {
+        return res.status(400).json({
+            succes: false,
+            error: 'You must provide a body',
+        })
+    }
+
+    findMany({ quiz: req.params.quiz }, (err, question) => {
+        if (err) {
+            return res.status(404).json({
+                err,
+                message: 'Quiz not found',
+            })
+        }
+        question.quiz = body.quiz;
+        question.save().then(() => {
+            return res.status(200).json({
+                success: true,
+                id: question._id,
+                message: 'Question updated',
+            })
+        }).catch(error => {
+            return res.status(404).json({
+                error,
+                message: 'Question not updated'
+            });
+        });
+    });
+
+    //or 
+    // Question.updateMany({
+    //     quiz: { $eq: req.params.quiz }}, 
+    //     { 
+    //         quiz: req.params.newQuiz 
+    //     }, 
+    //     function (err, docs) {
+    //     if (err) {
+    //         console.log(err);
+    //     } else {
+    //         console.log("Updated docs: ", docs);
+    //     }
+    // })
+}
+
+deleteQuestion = async (req, res) => {
+    await findOneAndDelete({ _id: req.params.id }, (err, question) => {
+        if (err) {
+            return res.status(400)
+        }
+
+        if (!question) {
+            return res.status(404).json({
+                success: false,
+                error: 'Question not found',
+            })
+        }
+        return res .status(200).json({ 
+            success: true,
+            data: question        
+        })
+    }).catch(err => console.log(err));
+}
+
+getQuestions = async (req, res) => {
+    await find({ quiz: req.params.quiz }, (err, questions) => {
+        if (err) {
+            return res.status(400).json({
+                success: false,
+                error: err
+            })
+        }
+        if (!questions.length) {
+            return res.status(404).json({
+                success: false,
+                error: 'Quiz not found'
+            })
+        }
+        return res.status(200).json({
+            succes: true,
+            data: questions
+        })
+    }).catch(err => console.log(err))
+}
+
+export default {
+    createQuestion,
+    updateQuestion,
+    updateQuizName,
+    deleteQuestion,
+    getQuestions,
+}
