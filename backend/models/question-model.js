d7d7ba57ddeb20765e9cdb905bce090b17af7277
Joe Ball <joe.ball@bglgroup.co.uk>
2020-10-28 14:55:39 +0000

    added question model, route and controller

---
+++ backend/models/question-model.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const Question = new Schema(
+    {
+        quiz: { type: String, required: true },
+        question: { type: String, required: true },
+        answer: { type: String, required: true }
+    }
+)
+
+module.exports = mongoose.model('questions', Question);
