27b4ce8f8311d6fb8e41fd3a0eded0ca46db5d23
Joe Ball <joe.ball@bglgroup.co.uk>
2020-11-19 15:26:44 +0000

    getting db data through to frontend

--- frontend/src/pages/QuizzesList.jsx
+++ frontend/src/pages/QuizzesList.jsx
@@ -1,63 +1,99 @@
 import React, { Component } from 'react';
-import ReactTable from 'react-table'
-import api from '../api';
+import axios from "axios";
 
-class QuizzesList extends Component {
+export default class QuizzesList extends Component {
     constructor(props) {
         super(props)
         this.state = {
             quizzes: [],
-            columns: [],
             isLoading: false
         }
     }
 
     componentDidMount = async () => {
         this.setState({ isLoading: true });
-        await api.getAllQuizzes().then(quizzes => {
-            console.log("quizzes: " + quizzes);
+
+        await axios.get("/api/quizzes").then(response => {
+            console.log(response.data.data);
             this.setState({
-                quizzes: quizzes.data["data"],
-                isLoading: false,
+                quizzes: response.data.data,
+                isLoading: false
             })
-            
-        })
-    }
+        });
+    };
 
     render() {
         const { quizzes, isLoading } = this.state;
-        console.log('TCL: QuizzesList -> render -> quizzes', quizzes);
-
-        const columns = [
-            {
-                Header: 'Quiz Name',
-                accessor: 'quiz',
-                filterable: true,
-            }
-        ]
-
-        let showTable = true;
-        if (!quizzes.length) {
-            showTable = false;
-        }
-
+        console.log(quizzes)
+        const quizNames = ["test", "test2"];
         return (
             <div>
-                this is where the quizzes table should go {quizzes}
-              {showTable && (
-                  <ReactTable
-                  data={quizzes}
-                  loading={isLoading}
-                  columns={columns}
-                  defaultPageSize={10}
-                  showPageSizeOptions={true}
-                  minRows={0}
-                  />
-            )}  
+                this is where the quizzes table should go: {quizNames}
             </div>
             
         )
     }
 }
 
-export default QuizzesList;
+// import React, { Component } from 'react';
+// import ReactTable from 'react-table'
+// import api from '../api';
+
+// class QuizzesList extends Component {
+//     constructor(props) {
+//         super(props)
+//         this.state = {
+//             quizzes: [],
+//             columns: [],
+//             isLoading: false
+//         }
+//     }
+
+//     componentDidMount = async () => {
+//         this.setState({ isLoading: true });
+
+//         await api.getAllQuizzes().then(quizzes => {
+//             this.setState({
+//                 quizzes: quizzes.data.data,
+//                 isLoading: false,
+//             })
+//         })
+//     }
+
+//     render() {
+//         const { quizzes, isLoading } = this.state;
+//         console.log('TCL: QuizzesList -> render -> quizzes', quizzes);
+
+//         const columns = [
+//             {
+//                 Header: 'Quiz Name',
+//                 accessor: 'quiz',
+//                 filterable: true,
+//             }
+//         ]
+
+//         let showTable = true;
+//         if (!quizzes.length) {
+//             showTable = false;
+//         }
+
+//         return (
+//             <div>
+//                 this is where the quizzes table should go {quizzes}
+//               {showTable && (
+//                   <ReactTable
+//                   data={quizzes}
+//                   loading={isLoading}
+//                   columns={columns}
+//                   defaultPageSize={10}
+//                   showPageSizeOptions={true}
+//                   minRows={0}
+//                   />
+//             )}  
+//             </div>
+            
+//         )
+//     }
+// }
+
+// export default QuizzesList;
