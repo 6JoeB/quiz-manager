18cb46f18506b7ae71a730a6bf1c67e02df47d12
Joe Ball <joe.ball@bglgroup.co.uk>
2020-11-30 17:08:45 +0000

    updated on hover colour

--- frontend/src/components/LogoutButton.jsx
+++ frontend/src/components/LogoutButton.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useAuth0 } from '@auth0/auth0-react';
+import '../App.css';
 
 const LogoutButton = () => {
     const { logout, isAuthenticated, user } = useAuth0();
@@ -10,7 +11,6 @@ const LogoutButton = () => {
                 <a className="nav-link" onClick={() => logout()}>
                     {user.nickname}/Logout
                 </a>
-                
             </>
         )
     )
