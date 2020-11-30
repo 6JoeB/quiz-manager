211454670e6526dbbf017f84f4cad8c7c7bfbe66
Joe Ball <joe.ball@bglgroup.co.uk>
2020-11-30 16:40:15 +0000

    added homepage welcome gif and styled q&a list buttons

--- frontend/src/pages/Homepage.jsx
+++ frontend/src/pages/Homepage.jsx
@@ -6,7 +6,8 @@ export default class HomePage extends Component {
         return (
             <div className="text__title">
                 <h2>Welcome to the Quiz Manager</h2>
-                <p>Please use the nav bar above to guide yourself around the site</p>
+                <p>Please use the nav bar above to guide yourself around the site.</p>
+                <iframe src="https://giphy.com/embed/bcKmIWkUMCjVm"  class="giphy-embed welcome-gif" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/animated-hello-waving-bcKmIWkUMCjVm"></a></p>
             </div>
         )
     }
